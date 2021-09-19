725b94a78707769e0103b303426f10aee93a01af
bulutharunmurat <bulutharunmurat@gmail.com>
2021-09-19 21:55:54 +0300

    Add CustomerDTO

---
+++ CreditScoreCalculator/src/main/java/dev/patika/creditscorecalculator/DTO/CustomerDTO.java
@@ -0,0 +1,36 @@
+package dev.patika.creditscorecalculator.DTO;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.NumberFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerDTO {
+
+    @ApiModelProperty(example = "12345678902")
+    @NotNull(message = "SSID is mandatory")
+    @NumberFormat(style = NumberFormat.Style.NUMBER)
+    private Long ssid;
+
+    @ApiModelProperty(example = "Harun Murat Bulut")
+    @NotBlank(message = "Name is mandatory!!!")
+    private String customerName;
+
+    @ApiModelProperty(example = "5000.0")
+    @NumberFormat(style = NumberFormat.Style.NUMBER)
+    private Float customerSalary;
+
+    @ApiModelProperty(example = "05310123456")
+    @NotBlank
+    private String customerPhoneNumber;
+
+}
